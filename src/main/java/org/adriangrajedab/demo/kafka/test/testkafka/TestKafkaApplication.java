7c5e6abb26ebff238049c89e76d11d28ddeb0514
adrian.grajeda <adrian.grajeda@unosquare.com>
2023-03-01 23:31:01 -0400

    Initial commit

---
+++ src/main/java/org/adriangrajedab/demo/kafka/test/testkafka/TestKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.adriangrajedab.demo.kafka.test.testkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/adriangrajedab/demo/kafka/test/testkafka/TestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adriangrajedab.demo.kafka.test.testkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
